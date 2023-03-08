874e8db7e7c9fcf75d3d00dfc9b9543f3a2c61d7
david <dcchabgood@gmail.com>
2023-03-09 00:04:00 +1000

    Add test mode env var to conftest.py

--- tests/vocprez/conftest.py
+++ tests/vocprez/conftest.py
@@ -2,6 +2,7 @@ import os
 import sys
 from pathlib import Path
 
+os.environ["TEST_MODE"] = "true"
 os.environ["VOCPREZ_SPARQL_ENDPOINT"] = "http://localhost:3031/vocprez"
 PREZ_DIR = Path(__file__).parent.parent.parent.parent.absolute() / "prez"
 os.environ["PREZ_DIR"] = str(PREZ_DIR)
