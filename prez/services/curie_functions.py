abfc272c863880aef9c1744fc8df59d519b89eee
David Habgood <dcchabgood@gmail.com>
2023-04-21 09:04:20 +1000

    David/curie bugfix (#96)
    * 1. Curie generation bugfix
    
    * 1. Add minimal docker-compose.yml example
    
    * Add tests for curie generation
    
    * Add curie tests to github action
    
    * Update docstring, parameterise positive test

--- prez/services/curie_functions.py
+++ prez/services/curie_functions.py
@@ -35,45 +35,53 @@ def generate_new_prefix(uri):
     Generates a new prefix for a uri
     """
     parsed_url = urlparse(uri)
+    to_generate_prefix_from = ""
     if bool(parsed_url.fragment):
         ns = f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}#"
         to_generate_prefix_from = parsed_url.fragment.lower()
     else:
         ns = f'{parsed_url.scheme}://{parsed_url.netloc}{"/".join(parsed_url.path.split("/")[:-1])}/'
-        try:
-            to_generate_prefix_from = parsed_url.path.split("/")[-2].lower()
-        except Exception:
-            log.error(
-                msg=f"Cannot generate a prefix for the URI {uri}, please specify a prefix in any of the "
-                f"prez/reference_data/prefixes turtle files"
-            )
-            raise
-    # attempt to just use the last part of the path prior to the fragment or "identifier"
-    if len(to_generate_prefix_from) <= 6:
-        proposed_prefix = to_generate_prefix_from
+        split_path = parsed_url.path.split("/")
+        if len(split_path) > 1:
+            to_generate_prefix_from = split_path[-2].lower()
+    if to_generate_prefix_from:
+        # attempt to just use the last part of the path prior to the fragment or "identifier"
+        if len(to_generate_prefix_from) <= 6:
+            proposed_prefix = to_generate_prefix_from
+            if not prefix_registered(proposed_prefix):
+                prefix_graph.bind(proposed_prefix, ns)
+                return
+        # otherwise, remove vowels to reduce length
+        proposed_prefix = "".join(
+            [c for c in to_generate_prefix_from if c not in "aeiou"]
+        )
         if not prefix_registered(proposed_prefix):
             prefix_graph.bind(proposed_prefix, ns)
             return
-    # otherwise, remove vowels to reduce length
-    proposed_prefix = "".join([c for c in to_generate_prefix_from if c not in "aeiou"])
-    if not prefix_registered(proposed_prefix):
-        prefix_graph.bind(proposed_prefix, ns)
-        return
     else:
-        # use RDFLib's default ns0, ns1 etc.
-        prefix_graph.compute_qname(uri, generate=True)
+        raise ValueError("Couldn't generate a prefix for the URI")
 
 
 def get_curie_id_for_uri(uri: URIRef):
     """
-    Returns a CURIE with ":" replaced by a given separator, for a given URI
+    This function gets a curie ID for a given URI.
+    The following process is used:
+    1. Check Prez's in memory prefix graph for an existing prefix for the URI's namespace.
+    2. If not found, attempt to generate a "nice" prefix using prez's "generate_new_prefix" function.
+    3. If unable to generate a "nice" prefix, use the "compute_qname" function to generate a prefix in the series ns0,
+    ns1 etc.
     """
     separator = settings.curie_separator
     try:
         qname = prefix_graph.compute_qname(uri, generate=False)
     except Exception:
-        generate_new_prefix(uri)  # this should always manage to generate a prefix.
-        qname = prefix_graph.compute_qname(uri, generate=False)
+        try:
+            generate_new_prefix(
+                uri
+            )  # this will mostly succeed in generating new prefixes.
+        except ValueError:
+            pass  # generation failed; function below will generate namespaces in the series ns0, ns1 etc.
+        qname = prefix_graph.compute_qname(uri, generate=True)
     return f"{qname[0]}{separator}{qname[2]}"
 
 
