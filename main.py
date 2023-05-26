76e1dc7821b688054eb4da9fee15fc3add136287
Edmond <edmond.chuc@outlook.com>
2023-05-26 11:41:19 +1000

    Set reload to true on uvicorn server to enable hot reload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import uvicorn
 from dotenv import load_dotenv
 
-# Load config from .env file.
-# See .env-template and prez/config.py for usage.
-load_dotenv()
+if __name__ == "__main__":
+    # Load config from .env file.
+    # See .env-template and prez/config.py for usage.
+    load_dotenv()
 
-uvicorn.run("prez.app:app", port=4000)
+    uvicorn.run("prez.app:app", port=4000, reload=True)
