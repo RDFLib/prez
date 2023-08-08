62a2f0c7a8fc3b1b072563245f04b5ef3e80c5e2
Edmond Chuc <edmond@kurrawong.ai>
2023-08-08 14:33:09 +1000

    Update test to ensure headers are forwarded on. Mainly used for content negotiation with accept header.

--- tests/identifier/test_redirect_endpoint.py
+++ tests/identifier/test_redirect_endpoint.py
@@ -28,31 +28,42 @@ def test_client(request):
 
 
 @pytest.mark.parametrize(
-    "iri, url, expected_response_code",
+    "iri, url, expected_response_code, accept_header_value",
     [
         [
             "http://data.bgs.ac.uk/id/dataHolding/13603129",
             "http://metadata.bgs.ac.uk/geonetwork/srv/eng/catalog.search#/metadata/9df8df53-2a1d-37a8-e044-0003ba9b0d98",
             307,
+            "",
         ],
+        ["http://example.com/non-existent", None, 404, ""],
         [
-            "http://example.com/non-existent",
-            None,
-            404,
+            "http://data.bgs.ac.uk/id/dataHolding/13603129",
+            "http://metadata.bgs.ac.uk/geonetwork/srv/eng/catalog.search#/metadata/9df8df53-2a1d-37a8-e044-0003ba9b0d98",
+            307,
+            "text/turtle",
         ],
     ],
 )
 def test_redirect_endpoint(
-    test_client: TestClient, iri: str, url: str, expected_response_code
+    test_client: TestClient,
+    iri: str,
+    url: str,
+    expected_response_code,
+    accept_header_value: str | None,
 ):
     params = {"iri": iri}
+    headers = {"accept": accept_header_value}
     response = test_client.get(
-        "/identifier/redirect", params=params, follow_redirects=False
+        "/identifier/redirect", params=params, headers=headers, follow_redirects=False
     )
 
     if expected_response_code != 404:
         assert response.status_code == expected_response_code
         assert response.headers.get("location") == url
+
+        if accept_header_value:
+            assert response.headers.get("accept") == accept_header_value
     else:
         assert response.status_code == expected_response_code
         assert response.headers.get("content-type") == "application/json"
