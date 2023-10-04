1e1e9bf00591fbd449ef6eb39b0c5eeda69466b2
david <dcchabgood@gmail.com>
2023-10-04 12:00:41 +1000

    Add basic test for predicate listing endpoints

--- prez/routers/management.py
+++ prez/routers/management.py
@@ -1,10 +1,14 @@
 from connegp import RDF_MEDIATYPES
 from fastapi import APIRouter
+from rdflib import Graph, BNode, Literal
+from rdflib.collection import Collection
 from starlette.requests import Request
 from starlette.responses import PlainTextResponse
 
+from prez.reference_data.prez_ns import PREZ
+from prez.config import settings
 from prez.cache import tbox_cache
-from prez.renderers.renderer import return_rdf
+from prez.renderers.renderer import return_rdf, return_from_graph
 from prez.services.app_service import add_common_context_ontologies_to_tbox_cache
 
 router = APIRouter(tags=["Management"])
@@ -26,3 +30,20 @@ async def return_tbox_cache(request: Request):
     if not mediatype or mediatype not in RDF_MEDIATYPES:
         mediatype = "text/turtle"
     return await return_rdf(tbox_cache, mediatype, profile_headers={})
+
+
+@router.get("/annotation-predicates", summary="Show the Annotation Predicates")
+async def return_annotation_predicates():
+    """
+    Returns an RDF linked list of the annotation predicates used for labels, descriptions and provenance.
+    """
+    g = Graph()
+    g.bind("prez", "https://prez.dev/")
+    label_list_bn, description_list_bn, provenance_list_bn = BNode(), BNode(), BNode()
+    g.add((PREZ.AnnotationPropertyList, PREZ.labelList, label_list_bn))
+    g.add((PREZ.AnnotationPropertyList, PREZ.descriptionList, description_list_bn))
+    g.add((PREZ.AnnotationPropertyList, PREZ.provenanceList, provenance_list_bn))
+    Collection(g, label_list_bn, settings.label_predicates)
+    Collection(g, description_list_bn, settings.description_predicates)
+    Collection(g, provenance_list_bn, settings.provenance_predicates)
+    return await return_from_graph(g, "text/turtle", PREZ["profile/open"], {})
