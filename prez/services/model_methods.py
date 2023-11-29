d9f1e5e19ddc2deaeb3e37edac1eb3a242af1557
David Habgood <dcchabgood@gmail.com>
2023-11-29 17:25:02 +1000

    Fix broken profiles (#185)
    black code

--- prez/services/model_methods.py
+++ prez/services/model_methods.py
@@ -17,9 +17,12 @@ async def get_classes(
     _, r = await repo.send_queries([], [(uri, q)])
     tabular_result = r[0]  # should only be one result - only one query sent
     if endpoint != URIRef("https://prez.dev/endpoint/object"):
-        endpoint_classes = list(endpoints_graph_cache.objects(
-            subject=endpoint, predicate=URIRef("https://prez.dev/ont/deliversClasses")
-        ))
+        endpoint_classes = list(
+            endpoints_graph_cache.objects(
+                subject=endpoint,
+                predicate=URIRef("https://prez.dev/ont/deliversClasses"),
+            )
+        )
         object_classes_delivered_by_endpoint = []
         for c in tabular_result[1]:
             if URIRef(c["class"]["value"]) in endpoint_classes:
