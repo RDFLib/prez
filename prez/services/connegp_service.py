e53899408036e9adde6b0a8d1f10f72f1d20fea0
Lawson Lewis <lawson@kurrawong.ai>
2024-02-20 11:23:00 +1000

    WIP need to implement token resolution.

--- prez/services/connegp_service.py
+++ prez/services/connegp_service.py
@@ -4,7 +4,15 @@ import re
 from pydantic import BaseModel
 
 
+class TokenError(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
 class ConnegpParser(BaseModel):
+    """An implementation of the Content Negotiation by Profile Standard.
+    See: https://w3c.github.io/dx-connegp/connegp/#introduction
+    """
     headers: dict
     params: dict
     default_weighting: float = 1.0
@@ -12,11 +20,26 @@ class ConnegpParser(BaseModel):
     requested_mediatypes: list[tuple[str, float]] | None = None
 
     class Config:
+        # Enable coercion of Starlette Headers and QueryParams to dict.
         strict = False
 
-    def _tupilize(self, string) -> tuple[str, float]:
-        parts = string.split("q=")
-        parts[0] = parts[0].strip(" ;<>")
+    @staticmethod
+    def _resolve_token(token: str) -> str:
+        # TODO: implement token resolution logic
+        #       "there's a system_repo: Repo = Depends(get_system_repo)
+        #       which you can SPARQL query to go from token -> uri"
+        raise TokenError("invalid token")
+
+    def _tupilize(self, string, is_profile: bool = False) -> tuple[str, float]:
+        parts = string.split("q=")  # split out the weights
+        parts[0] = parts[0].strip(" ;")  # remove seperator character, and whitespace
+        if is_profile and not re.search(r"^<.*>$", parts[0]):  # if it doesn't look like a URI
+            try:
+                parts[0] = self._resolve_token(parts[0])
+            except TokenError as e:
+                log = logging.getLogger("prez")
+                log.error(
+                    f"Could not resolve URI for token '{parts[0]}': {e.args[0]}")
         if len(parts) == 1:
             parts.append(self.default_weighting)
         else:
@@ -25,27 +48,27 @@ class ConnegpParser(BaseModel):
             except ValueError as e:
                 log = logging.getLogger("prez")
                 log.debug(
-                    f"Could not cast q={parts[1]} as float. Defaulting to {self.default_weighting}")
+                    f"Could not cast q={parts[1]} as float. Defaulting to {self.default_weighting}. {e.args[0]}")
         return parts[0], parts[1]
 
     @staticmethod
-    def _prioritize(types: list[tuple[str, float]]):
+    def _prioritize(types: list[tuple[str, float]]) -> list[tuple[str, float]]:
         return sorted(types, key=lambda x: x[1], reverse=True)
 
     def _parse_mediatypes(self) -> None:
-        header_mediatypes: str = self.headers.get("Accept", "")
-        qsa_mediatypes: str = self.params.get("_media", "")
-        mediatypes = re.sub(r",$", "", "".join([qsa_mediatypes, header_mediatypes]))
-        if mediatypes:
-            mediatypes = [self._tupilize(mediatype) for mediatype in mediatypes.split(",")]
+        raw_mediatypes: str = self.params.get("_media", "")
+        if not raw_mediatypes:
+            raw_mediatypes: str = self.headers.get("Accept", "")
+        if raw_mediatypes:
+            mediatypes: list = [self._tupilize(mediatype) for mediatype in raw_mediatypes.split(",")]
             self.requested_mediatypes = self._prioritize(mediatypes)
 
     def _parse_profiles(self) -> None:
-        header_profiles: str = self.headers.get("Accept-Profile", "")
-        qsa_profiles: str = self.params.get("_profile", "")
-        profiles = re.sub(r",$", "", "".join([qsa_profiles, header_profiles]))
-        if profiles:
-            profiles = [self._tupilize(mediatype) for mediatype in profiles.split(",")]
+        raw_profiles: str = self.params.get("_profile", "")
+        if not raw_profiles:
+            raw_profiles: str = self.headers.get("Accept-Profile", "")
+        if raw_profiles:
+            profiles: list = [self._tupilize(profile, is_profile=True) for profile in raw_profiles.split(",")]
             self.requested_profiles = self._prioritize(profiles)
 
     def get_requested_profiles(self) -> list[tuple[str, float]] | None:
--- tests/test_connegp.py
+++ tests/test_connegp.py
@@ -9,14 +9,38 @@ from prez.services.connegp_service import ConnegpParser
         [
             {"Accept-Profile": "<default>, <alternate>;q=0.9"},
             {"_media": "text/anot+turtle, text/turtle;q=0.9"},
-            [("default", 1.0), ("alternate", 0.9)],
-            [("text/anot+turtle", 1.0), ("text/turtle", 0.9)]
+            [("<default>", 1.0), ("<alternate>", 0.9)],
+            [("text/anot+turtle", 1.0), ("text/turtle", 0.9)]  # Test that profiles/mediatypes are extracted
         ],
         [
             {"Accept-Profile": "<alternate>;q=0.9, <default>"},
             {"_media": "text/turtle;q=0.9, text/anot+turtle"},
-            [("default", 1.0), ("alternate", 0.9)],
-            [("text/anot+turtle", 1.0), ("text/turtle", 0.9)]
+            [("<default>", 1.0), ("<alternate>", 0.9)],
+            [("text/anot+turtle", 1.0), ("text/turtle", 0.9)]  # Test that they are prioritized correctly
+        ],
+        [
+            {"Accept": "application/json"},
+            {"_media": "text/turtle"},
+            None,
+            [("text/turtle", 1.0)]  # Test that QSA is preferred over HTTP for mediatypes
+        ],
+        [
+            {"Accept-Profile": "<default>"},
+            {"_profile": "<alternate>"},
+            [("<alternate>", 1.0)],
+            None  # Test that QSA is preferred over HTTP for profiles
+        ],
+        [
+            {"Accept-Profile": "invalid-token"},
+            {},
+            [("invalid-token", 1.0)],
+            None  # Test that an unresolvable token is returned as is
+        ],
+        [
+            {"Accept-Profile": "ogc"},
+            {},
+            [("<https://prez.dev/OGCRecordsProfile>", 1.0)],
+            None  # Test that a resolvable token is resolved
         ]
     ]
 )
