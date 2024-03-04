2e612e35a987f150b32ca47a87ff894fa82a45a4
Lawson Lewis <lawson@kurrawong.ai>
2024-03-04 10:43:52 +1000

    fix incorrect relative file paths in test suite

--- tests/test_alt_profiles.py
+++ tests/test_alt_profiles.py
@@ -18,7 +18,7 @@ def test_store() -> Store:
     # Create a new pyoxigraph Store
     store = Store()
 
-    file = Path("../test_data/catprez.ttl")
+    file = Path(__file__).parent.parent / "test_data/catprez.ttl"
     store.load(file.read_bytes(), "text/turtle")
 
     return store
