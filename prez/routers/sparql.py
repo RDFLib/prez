343a2694a32478af60be8f8a9e4c3dd6a4bd0429
Ashley Sommer <ashleysommer@gmail.com>
2024-07-01 20:36:55 +1000

    Don't stream the proxied /sparql response to the client if the response has an attachment, this breaks when Prez is exposed through Azure APIM, because APIM strips out the attached file. Instead we wait for the whole response with attached file, read it, then send the response.

--- prez/routers/sparql.py
+++ prez/routers/sparql.py
@@ -65,19 +65,40 @@ async def sparql_endpoint_handler(query: str, request: Request, repo: Repo, meth
             media_type=non_anot_mediatype,
             headers=prof_and_mt_info.profile_headers,
         )
+
+    query_result: 'httpx.Response' = await repo.sparql(query, request.headers.raw, method=method)
+    if isinstance(query_result, dict):
+        return JSONResponse(content=query_result)
+    elif isinstance(query_result, Graph):
+        return Response(
+            content=query_result.serialize(format="text/turtle"),
+            status_code=200
+        )
+
+    dispositions = query_result.headers.get_list("Content-Disposition")
+    for disposition in dispositions:
+        if disposition.lower().startswith("attachment"):
+            is_attachment = True
+            break
     else:
-        query_result = await repo.sparql(query, request.headers.raw, method=method)
-        if isinstance(query_result, dict):
-            return JSONResponse(content=query_result)
-        elif isinstance(query_result, Graph):
-            return Response(
-                content=query_result.serialize(format="text/turtle"),
-                status_code=200
-            )
-        else:
-            return StreamingResponse(
-                query_result.aiter_raw(),
-                status_code=query_result.status_code,
-                headers=dict(query_result.headers),
-                background=BackgroundTask(query_result.aclose),
-            )
+        is_attachment = False
+    if is_attachment:
+        # remove transfer-encoding chunked, disposition=attachment, and content-length
+        headers = dict()
+        for k, v in query_result.headers.items():
+            if k.lower() not in ("transfer-encoding", "content-disposition", "content-length"):
+                headers[k] = v
+        content = await query_result.aread()
+        await query_result.aclose()
+        return Response(
+            content=content,
+            status_code=query_result.status_code,
+            headers=headers,
+        )
+    else:
+        return StreamingResponse(
+            query_result.aiter_raw(),
+            status_code=query_result.status_code,
+            headers=dict(query_result.headers),
+            background=BackgroundTask(query_result.aclose),
+        )
