80e73a00d2c4dbede4348a74e8040f5797efa589
David Habgood <dcchabgood@gmail.com>
2024-07-02 13:54:53 +1000

    Add retrieval of remote prefix declarations. Not yet passing with local pytest tests. Tested to be working manually with Fuseki. Local test failing may be resolved with changes to tests in V4. (#240)

---
+++ tests/test_remote_prefixes.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import pytest
+from fastapi.testclient import TestClient
+from pyoxigraph.pyoxigraph import Store
+from rdflib import Graph, URIRef
+from rdflib.namespace import RDF, DCAT
+
+from prez.app import assemble_app
+from prez.dependencies import get_repo
+from prez.sparql.methods import Repo, PyoxigraphRepo
+
+
+@pytest.fixture(scope="session")
+def test_store() -> Store:
+    # Create a new pyoxigraph Store
+    store = Store()
+
+    for file in Path(__file__).parent.glob("../tests/data/prefixes/*.ttl"):
+        store.load(file.read_bytes(), "text/turtle")
+
+    return store
+
+
+@pytest.fixture(scope="session")
+def test_repo(test_store: Store) -> Repo:
+    # Create a PyoxigraphQuerySender using the test_store
+    return PyoxigraphRepo(test_store)
+
+
+@pytest.fixture(scope="session")
+def client(test_repo: Repo) -> TestClient:
+    # Override the dependency to use the test_repo
+    def override_get_repo():
+        return test_repo
+
+    app = assemble_app()
+
+    app.dependency_overrides[get_repo] = override_get_repo
+
+    with TestClient(app) as c:
+        yield c
+
+    # Remove the override to ensure subsequent tests are unaffected
+    app.dependency_overrides.clear()
+
+
+@pytest.mark.xfail(
+    reason="Dependency overrides not configured correctly. Test passes when manually tested using Fuseki"
+)
+def test_catalog_link(client):
+    # get link for first catalog
+    r = client.get("/c/catalogs")
+    g = Graph().parse(data=r.text)
+    member_uri = g.value(None, RDF.type, DCAT.Catalog)
+    link = str(g.value(member_uri, URIRef(f"https://prez.dev/link", None)))
+    assert link == "/c/catalogs/davo:bogusCatalogous"
