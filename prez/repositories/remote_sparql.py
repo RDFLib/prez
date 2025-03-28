ceb37a7f777f96b039862ced5093c048cbda2f46
Lawon Lewis <lawson@kurrawong.ai>
2025-03-28 11:34:46 +1000

    fix: don't assume 'nt' mediatype from remote sparql endpoint
    previous implementation was hard coded to assume n-triples responses
    from the remote_sparql endpoint. This update allows the content-type to
    be dynamically discovered from the response and should make RDF request
    parsing more robust.
    
    still falls back to n-triples if not content-type is given in the
    response.

--- prez/repositories/remote_sparql.py
+++ prez/repositories/remote_sparql.py
@@ -44,8 +44,9 @@ class RemoteSparqlRepo(Repo):
         """
         response = await self._send_query(query)
         g = Graph()
+        format = response.headers.get("content-type", "nt")
         await response.aread()
-        return g.parse(data=response.text, format="nt")
+        return g.parse(data=response.text, format=format)
 
     async def tabular_query_to_table(self, query: str, context: URIRef = None):
         """
