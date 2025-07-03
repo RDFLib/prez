2c06fb922e64bf3c1dd95fdbe2d66a48ccba0a7f
Edmond Chuc <edmond@kurrawong.ai>
2025-07-03 14:58:25 +1000

    test: concept scheme inverse path skos:inScheme

--- tests/test_ogc_skos_collection_member_object_profile.py
+++ tests/test_ogc_skos_collection_member_object_profile.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from rdflib import Graph
+from rdflib import Graph, URIRef, SKOS
 from prez.reference_data.prez_ns import PREZ
 
 
@@ -11,3 +11,24 @@ def test_skos_collection_member_object_profile(client: TestClient):
         PREZ.currentProfile,
         PREZ.OGCSKOSCollectionObjectProfile,
     ) in response_graph
+
+
+def test_skos_concept_scheme_object_profile_returns_incoming_in_scheme_from_skos_collection(
+    client: TestClient,
+):
+    r = client.get("/object?uri=https://linked.data.gov.au/def/road-types")
+    response_graph = Graph().parse(data=r.text)
+
+    # /qld is a skos:Collection, it should exist in the response.
+    assert (
+        URIRef("https://linked.data.gov.au/def/road-types/qld"),
+        SKOS.inScheme,
+        URIRef("https://linked.data.gov.au/def/road-types"),
+    ) in response_graph
+
+    # /yard is a skos:Concept, it should not exist in the response.
+    assert (
+        URIRef("https://linked.data.gov.au/def/road-types/yard"),
+        SKOS.inScheme,
+        URIRef("https://linked.data.gov.au/def/road-types"),
+    ) not in response_graph
